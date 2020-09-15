1eff459b8a4dd018b0be09f60ac3fbe64d0fd0a6
unknown <henrique.dcti@gmail.com>
2020-09-15 17:49:10 -0300

    added first component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Grid } from './styles';
+
+// import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Grid>
+      
+    </Grid>
+  )
+}
+
+export default Layout;
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const ServerList: React.FC = () => {
+  return <div />;
+}
+
+export default ServerList;
