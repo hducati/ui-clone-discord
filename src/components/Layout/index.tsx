d7577d237b6c8ce4d814f626598c165a1347c6d7
unknown <henrique.dcti@gmail.com>
2020-09-23 00:39:17 -0300

    added server list component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Grid } from './styles';
 
+import ServerList from '../ServerList';
+
 // import { Container } from './styles';
 
 const Layout: React.FC = () => {
   return (
     <Grid>
-      
+      <ServerList/>
     </Grid>
   )
 }
--- src/components/ServerList/index.tsx
+++ src/components/ServerList/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Separator } from './styles';
 
 const ServerList: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <ServerButton isHome/>
+
+      <Separator/>
+
+      <ServerButton/>
+      <ServerButton hasNotifications/>
+      <ServerButton mentions={3}/>
+      <ServerButton/>
+      <ServerButton/>
+      <ServerButton hasNotifications/>
+      <ServerButton/>
+      <ServerButton mentions={2}/>
+      <ServerButton/>
+      <ServerButton/>
+      <ServerButton/>
+
+    </Container>
+  )
 }
 
 export default ServerList;
