f6fb502fcac826071b40f393987ac1b71426af34
unknown <henrique.dcti@gmail.com>
2020-09-27 13:25:34 -0300

    added channel components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Grid } from './styles';
 
 import ServerList from '../ServerList';
+import ServerName from '../ServerName'
+import ChannelInfo from '../ChannelInfo';
+import ChannelList from '../ChannelList'
 
 // import { Container } from './styles';
 
@@ -10,6 +13,9 @@ const Layout: React.FC = () => {
   return (
     <Grid>
       <ServerList/>
+      <ServerName/>
+      <ChannelInfo/>
+      <ChannelList/>
     </Grid>
   )
 }
